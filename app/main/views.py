683443a7feec6157ae8c82666122f469cc9712f8
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-22 15:47:40 +0300

    added a search view in tha main folder

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from ..requests import get_books,get_book,search_book
 
 
 @main.route('/')
@@ -11,4 +12,15 @@ def index():
 
 
     return render_template('index.html')
+
+@main.route('/search/<book_name>')
+def search(book_name):
+    '''
+    View function to display the search results
+    '''
+    book_name_list = book_name.split(" ")
+    book_name_format = "+".join(book_name_list)
+    searched_books = search_book(book_name_format)
+    title = f'search results for {book_name}'
+    return render_template('search.html',books = searched_books)
 
