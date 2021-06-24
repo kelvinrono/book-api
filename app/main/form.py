3366e72edf55bfe032c90ab52129764ef0deab16
TechVictorKE <victor.maina@student.moringaschool.com>
2021-06-24 22:12:30 +0300

    Resolved conflicts

--- app/main/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
-from wtforms.validators import Required,Email
-from ..models import User
-
-class ReviewForm(FlaskForm):
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
-    submit = SubmitField('Submit')
