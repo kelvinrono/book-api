7f05176967ff9e57c4ef86a85254060f4daea942
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-06-24 16:09:33 +0300

    added a SQLAQMY BASE URL

--- config.py
+++ config.py
@@ -5,10 +5,7 @@ class Config:
     BOOK_API_BASE_URL ='https://api.nytimes.com/svc/books/v3/lists/{}/hardcover-fiction.json?api-key={}'
     BOOK_API_KEY = os.environ.get('BOOK_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-<<<<<<< HEAD
-    SQLALCHEMY_DATABASE_URI = ''
-=======
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kelvin@localhost/book'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/book'
  
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -17,7 +14,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
->>>>>>> c53debbc4d04ce128ef7cfa2543841917b3687ef
+
 class ProdConfig(Config):
     pass
 
