9ed13a0085f04fae99e96f7ba401214ec25e5358
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-24 10:54:06 +0300

    updated the requests and the main views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,6 @@
 from flask import render_template,request,redirect,url_for
-
-from flask import render_template
-from ..requests import get_books
 from . import main
-from ..requests import get_books
+from ..requests import get_books,search_book
 
 
 # Views
@@ -18,15 +15,6 @@ def index():
 
     return render_template('index.html', current=current_books)
 
-@main.route('/book/<int:id>')
-def book(title):
-    '''
-    View book page function that returns the book details page and its data
-    '''
-    book = get_book(title)
-    title = f'{book.title}'
-    reviews = Review.get_reviews(book.title)
-    return render_template('book.html',title = title,book = book,reviews = reviews)
 @main.route('/search/<book_name>')
 def search(book_name):
     '''
